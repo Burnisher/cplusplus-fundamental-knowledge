acbb850925021f0e80566310d8fffed51809bffd
Ubuntu12.04 <test@ubuntu.(none)>
2014-11-10 16:28:07 +0800

    first try

---
+++ virtual_function.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+//虚函数表的实现
+class Base
+{
+public:
+    virtual void f() { cout << "Base::f" << endl; }
+    virtual void g() { cout << "Base::g" << endl; }
+    virtual void h() { cout << "Base::h" << endl; }
+};
+
+int main(int argc, const char *argv[])
+{
+    typedef void(*Fun) (void);
+    Base b;
+    Fun pFun = NULL;
+    cout << "vptr address:" << (int *)(&b) << endl;
+    cout << "虚函数表-第一个函数地址: " << (int*)*(int*)(&b) << endl;
+    pFun = (Fun)*((int*)*(int*)(&b));
+    pFun();
+    pFun = (Fun)*((int*)*(int*)(&b)+1);
+    pFun();
+    pFun = (Fun)*((int*)*(int*)(&b)+2);
+    pFun();
+    return 0;
+}
