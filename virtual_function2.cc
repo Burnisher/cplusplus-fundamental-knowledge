f74cebc79713c90f953166057649742b6c850280
Ubuntu12.04 <test@ubuntu.(none)>
2014-11-12 16:45:26 +0800

    virtual

--- virtual_function2.cc
+++ virtual_function2.cc
@@ -4,9 +4,18 @@
 using namespace std;
 //虚基类表指针（bptr）
 //虚基类只存在一个实体
+//虚基类继承时构造函数的调用
 
 
-class X { int a; char b; char c; };
+class X 
+{ 
+private:
+    int a; 
+    char b; 
+    char c; 
+public:
+    X() { cout << "construct X" << endl; }
+};
 class Y:public virtual X{};
 class Z:public virtual X{};
 class A:public Y, public Z{ char m;};
@@ -17,6 +26,10 @@ int main(int argc, const char *argv[])
     cout << "sizeof(Y) : " << sizeof(Y) << endl;
     cout << "sizeof(Z) : " << sizeof(Z) << endl;
     cout << "sizeof(A) : " << sizeof(A) << endl;
+
+    Y y;
+    Z z;
+    A a;
     return 0;
 }
 
